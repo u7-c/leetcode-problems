9f2948d5942683754d6c3d241fc1f9c312a09348
u7-c <aryansuneja121@gmail.com>
2025-01-12 18:50:39 +0530

    2d array full

---
+++ 2darray/binarysearch2darray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> >& matrix, int target) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int start = 0;
+        int end  = row*col-1;
+        
+        int mid = start + (end-start)/2;
+        
+        while(start<=end) {
+            
+            int element = matrix[mid/col][mid%col];
+            
+            if(element == target) {
+                return 1;
+            }
+            
+            if(element < target) {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+             mid = start + (end-start)/2;    
+        }
+        return 0;
+    }
+};
+//al the binary search code is just same as before just line number 18 is important cuz it determins the row and column
---
+++ 2darray/search2darray2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> >& matrix, int target) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int rowIndex = 0;
+        int colIndex = col-1;
+        
+        while(rowIndex < row && colIndex>=0 ) {
+            int element = matrix[rowIndex][colIndex];
+            
+            if(element == target) {
+                return 1;
+            }
+            
+            if(element < target){
+                rowIndex++;
+            }
+            else
+            {
+                colIndex--;
+            }
+        }
+        return 0;
+    }
+};
+//leetcode problem
+//there was like a grid in which it was ascending  both horizontally and vertically
+// so what we did we thought by our own and there were 3 cases out which were like row++ and column--
+// vizualize it completely how these cases go on u will get the rythm 
