206836e2472f35aefb89f5f217f68b03726fb2d3
u7-c <aryansuneja121@gmail.com>
2025-02-24 22:55:03 +0530

    recursion 5 lectures
    dsa 10 day recursion series 5 lecture codes

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate Fibonacci number using recursion
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+    for (int i = 0; i < n; i++) {
+        cout << fibonacci(i) << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
