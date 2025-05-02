2f456bba189cff0174808904e890017bffa11b70
u7-c <aryansuneja121@gmail.com>
2025-05-02 22:58:27 +0530

    array tuf problem

--- arrays/removeduplicateelementarray.cpp
+++ arrays/removeduplicateelementarray.cpp
@@ -10,6 +10,19 @@ bool sortedarray(int arr[],int size){
     }
     return true;
 }
+int removeduplicate(int arr[],int size){
+    int j=0;
+    if(size==1 || size==0){
+        return size;
+    }
+    for(int i=0;i<size-1;i++){
+        if(arr[i]!=arr[i+1]){
+            arr[j++]=arr[i];
+        }
+    }
+    arr[j++]=arr[size-1];
+    return j;
+}
 
 int main() {
     int size;
@@ -21,5 +34,15 @@ int main() {
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
+    int temp;
     bool sorted = sortedarray(arr,size);
+    if (sorted == true){
+        int newsize=removeduplicate(arr,size);
+        for(int i=0;i<newsize;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+    else{
+        cout<<"the array is not sorted"<<endl;
+    }
 }
